543023c1fd76b10761c69b7c2f76a3674edfc794
Sidhaant Anand <sidhaant.anand@scotiabank.com>
2018-09-28 20:33:08 -0400

    added options in instructor.js

--- instructor.js
+++ instructor.js
@@ -4,6 +4,10 @@ let button_A_state = 0
 let slow_count = 0
 let fast_count = 0
 let normal_count = 0
+let A_count = 0
+let B_count = 0
+let C_count = 0
+let D_count = 0
 
 input.onButtonPressed(Button.A, () => {
     // starting questionaire
@@ -13,6 +17,12 @@ input.onButtonPressed(Button.A, () => {
     } else {
         button_A_state = 0
         radio.sendNumber(100)
+		
+		// draw a graph with the A,B,C,D content
+        A_count = 0
+        B_count = 0
+        C_count = 0
+        D_count = 0
     }
 })
 radio.onDataPacketReceived(({ receivedNumber }) => {
@@ -28,20 +38,34 @@ radio.onDataPacketReceived(({ receivedNumber }) => {
         normal_count--
     }
     //Fast
-    else if (receivedNumber == 12) {
+    if (receivedNumber == 12) {
         fast_count++
         slow_count--
     }
 
     // normal
-    else if (receivedNumber == 11) {
+    if (receivedNumber == 11) {
         normal_count++
         fast_count--
+    }
+
+    if(receivedNumber == 21) {
+        A_count++;
+    }
+
+    if(receivedNumber == 22) {
+        B_count++
+    }
+
+    if(receivedNumber == 23) {
+        C_count++
+    }
+
+    if(receivedNumber == 24) {
+        D_count++
     }
 
     let num_to_show = fast_count - slow_count
     basic.showNumber(num_to_show)
     serial.writeNumber(num_to_show)
 })
-
-

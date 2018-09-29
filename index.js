b3d0b7714f8f079d767603468b7d2af706f67e1c
Akash Chamaria <achamaria@live.com>
2018-09-29 13:14:44 -0400

    Pace db

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-var SerialPort = require("serialport"); 
-var port = new SerialPort("COM10", {
-	baudRate : 115200
-});
-
 var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://theaimers:winners@cluster0-c02uj.mongodb.net/test?retryWrites=true');
+mongoose.connect('mongodb+srv://theaimers:winners@cluster0-c02uj.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -13,11 +8,39 @@ db.once('open', function() {
 	// we're connected!
 });
 
+var SerialPort = require("serialport"); 
+var port = new SerialPort("COM10", {
+	baudRate : 115200
+});
+
+
+
 const Readline = SerialPort.parsers.Readline;
 const parser = new Readline();
 port.pipe(parser);
+
+var classKeyData = "";
+var paceData = ""; 
 parser.on('data', (data) => {
-	
+	console.log("Sid");
+	if(data.includes("Timestamp")){
+		classKeyData = data.substring(data.indexOf(" ")+1);
+		console.log("thisis times: " + classKeyData);
+	}
+	if(data.includes("Pace")){
+		paceData = data.substring(data.indexOf(" ")+1);
+		console.log("thisis pace: " + paceData);
+		if(paceData != ""){
+			var paceObject = new Pace({ classKey: classKeyData, pace: paceData, paceTime: (new Date()).toISOString() });
+			paceObject.save(function(error) {
+				console.log("object created");
+				if(error){
+					console.error(error);
+				}
+			});
+			pace = "";
+		}
+	}
 	console.log(data);
 });
 
@@ -29,11 +52,5 @@ var paceSchema = new mongoose.Schema({
 
 var Pace = mongoose.model('Pace', paceSchema);
 
-var paceObject = new Pace({ classKey: "classkey", pace: "0", paceTime: "pacekey" });
 
-paceObject.save(function(error) {
-	console.log("object created");
-	if(error){
-		console.error(error);
-	}
-});
+

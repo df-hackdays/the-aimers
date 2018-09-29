b2381f5faa1630d20a793e27d19c88cadfde0966
Akash Chamaria <achamaria@live.com>
2018-09-29 14:00:07 -0400

    bugs fixed

--- index.js
+++ index.js
@@ -23,16 +23,16 @@ var classKeyData = "";
 var qaKeyData = "";
 var paceData = ""; 
 parser.on('data', (data) => {
-
+	console.log(data);
 	if(data.includes("started")){
 		classKeyData = (new Date()).toISOString();
 	}
-	if(data.includes("Pace")){
+	if(data.includes("Pace") && !data.includes("Results")){
 		paceData = data.substring(data.indexOf(" ")+1);
 		if(paceData != ""){
 			var paceObject = new Pace({ classKey: classKeyData, pace: paceData, paceTime: (new Date()).toISOString() });
 			paceObject.save(function(error) {
-				console.log("object created");
+				console.log("pace object created");
 				if(error){
 					console.error(error);
 				}
@@ -43,20 +43,20 @@ parser.on('data', (data) => {
 	if(data.includes("qastarted")){
 		qaKeyData = (new Date()).toISOString();
 	}
-	if(data.includes(A) && data.includes(B) && data.includes(C) && data.includes(D)){
+	if(data.includes("A") && data.includes("B") && data.includes("C") && data.includes("D")){
 		var a_count = data.substring(data.indexOf("A")+1, data.indexOf("B"));
 		var b_count = data.substring(data.indexOf("B")+1, data.indexOf("C"));
 		var c_count = data.substring(data.indexOf("C")+1, data.indexOf("D"));
 		var d_count = data.substring(data.indexOf("D")+1);
 		var qaObject = new Qa({ classKey: classKeyData, questionKey: qaKeyData, opta: a_count, optb: b_count, optc: c_count, optd: d_count});
 		qaObject.save(function(error) {
-				console.log("object created");
-				if(error){
-					console.error(error);
-				}
-			});
+			console.log("qa object created");
+			if(error){
+				console.error(error);
+			}
+		});
 	}
-	console.log(data);
+	
 });
 
 var paceSchema = new mongoose.Schema({

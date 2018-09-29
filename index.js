4a6006702431faf9595dd3ea64b56f1c26ad16fd
Sidhaant Anand <sidhaant.anand@scotiabank.com>
2018-09-29 11:35:43 -0400

    index.js

--- index.js
+++ index.js
@@ -2,29 +2,8 @@ var SerialPort = require("serialport");
 var port = new SerialPort("COM4", {
 	baudRate : 115200
 });
-/*
-var serialPort = new SerialPort("COM4", {
-  baudrate: 115200,
-  //parser: serialport.parsers.readline("\n")
-});
-*/
 
-/*
-serialPort.on("open", function () {
-  console.log('open');
-  serialPort.on('data', function(data) {
-    console.log(data);
-  });
-});
-
-*/
-
-port.open(function(success) {
-	if(err) {
-		return console.log("Success " , success.message);
-	}
-});
-
-port.on('data', function (data) {
-  console.log(data.toString("utf-8"));
-});
+const Readline = SerialPort.parsers.Readline;
+const parser = new Readline();
+port.pipe(parser);
+parser.on('data', console.log);
